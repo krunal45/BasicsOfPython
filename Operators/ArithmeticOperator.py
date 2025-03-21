97fc8b3b5a9da590dde3a1d7406cf46339b4d1a5
qaprayogshala <qaprayogshala@gmail.com>
2025-03-21 15:25:34 +0530

    ArithmeticOperator code

---
+++ Operators/ArithmeticOperator.py
@@ -0,0 +1,7 @@
+def add(number1,number2):
+    return number1 + number2
+
+num1 = int(input('Please enter a number1: '))
+num2 = int(input('Please enter a number2: '))
+
+print(f'The sum of {num1} and {num2} is {add(num1, num2)}')
