ba79ac07ba154b6f735ceb7997ae0bc9e8dc21b9
qaprayogshala <qaprayogshala@gmail.com>
2025-04-08 15:46:27 +0530

    user Input Example

---
+++ userInput/example1.py
@@ -0,0 +1,7 @@
+num1 = int(input('Enter number 1:'))
+num2 = int(input('Enter number 2:'))
+res = num1 + num2
+print(f'{num1} + {num2} = {res}')
+
+name = input('Please Enter Name: ')
+print(f'name: {name}')
