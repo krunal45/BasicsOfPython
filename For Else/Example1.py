93694a6c888e27b34edfc9b20988f36c0a1cd55a
qaprayogshala <qaprayogshala@gmail.com>
2025-04-15 15:50:38 +0530

    For Else Code practice

---
+++ For Else/Example1.py
@@ -0,0 +1,17 @@
+# fruits = ['Grapes','Banana','Chickoo','Dragon Fruit']
+
+# for fruit in fruits:
+#     if fruit.startswith('A'):
+#         print(fruit)
+#         break
+# else:
+#     print('Fruit not found!')
+
+nums = [2,3,4,5,6]
+
+for num in nums:
+    if num %2 == 0:
+        print(num)
+        break
+else:
+    print('Number not found!')    
