0afd0ca69a204eec0d2ae52d227c635a1e33e031
qaprayogshala <qaprayogshala@gmail.com>
2025-04-15 15:45:03 +0530

    pattern Print code

---
+++ Loops/For Loop/Example4.py
@@ -0,0 +1,29 @@
+# Printing Pattern:
+# * * * *
+# * * * *
+# * * * *
+# * * * *
+
+# -- Pattern 1 --
+# for row in range(4):
+#     for col in range(4):
+#         print('* ',end="")
+#     print()
+
+# -- Pattern 2 --
+# for row in range(1,5):
+#     for col in range(row):
+#         print('* ',end="")
+#     print()
+
+# -- Pattern 3 --
+# for row in range(4,0,-1):
+#     for col in range(row):
+#         print('* ',end="")
+#     print()
+
+# -- Pattern 4 --
+# for row in range(4,0,-1):
+#     for col in range(row):
+#         print('* ',end="")
+#     print()
